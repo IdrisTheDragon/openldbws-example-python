fdf135181ac70aff789fc5fe2569030ffdd1399c
Nathan Williams <nathan.williams@codethink.co.uk>
2024-12-06 11:04:26 +0000

    add cli

--- getDepartureBoardExample.py
+++ getDepartureBoardExample.py
@@ -12,7 +12,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
+# You should have received a copy of the GNU General Public Licensec
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
@@ -23,33 +23,13 @@ from dotenv import load_dotenv
 import time
 import datetime
 import os
+import click
 
-load_dotenv()
-
-LDB_TOKEN = os.getenv("RAIL_TOKEN")
-WSDL = 'http://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx?ver=2021-11-01'
-
-START_ST = 'SWT'
-END_ST = 'MAN'
-
-if LDB_TOKEN == '':
-    raise Exception("Please configure your OpenLDBWS token in getDepartureBoardExample!")
-
-settings = Settings(strict=False)
-
-history = HistoryPlugin()
-
-client = Client(wsdl=WSDL, settings=settings, plugins=[history])
-
-header = xsd.Element(
-    '{http://thalesgroup.com/RTTI/2013-11-28/Token/types}AccessToken',
-    xsd.ComplexType([
-        xsd.Element(
-            '{http://thalesgroup.com/RTTI/2013-11-28/Token/types}TokenValue',
-            xsd.String()),
-    ])
-)
-header_value = header(TokenValue=LDB_TOKEN)
+def in_between(now, start, end):
+    if start <= end:
+        return start <= now < end
+    else:
+        return start <= now or now < end
 
 def displayStationBoard(res, destCRS):
     print(f"Trains from {res.locationName} to {res.filterLocationName}" )
@@ -69,35 +49,80 @@ def displayStationBoard(res, destCRS):
             callingPoints = t.subsequentCallingPoints.callingPointList[0].callingPoint
             for callP in callingPoints:
                 if callP.crs == destCRS:
-                   line += f" - dest {callP.st} ETA {callP.et}"
+                    line += f" - dest {callP.st} ETA {callP.et}"
         if t.cancelReason:
             line += f"\n{t.cancelReason}"
         print(line)
     print("===============================================================================")
 
-def in_between(now, start, end):
-    if start <= end:
-        return start <= now < end
-    else:
-        return start <= now or now < end
+class Dashboard:
+    def __init__(self,START_ST,END_ST):
+        self.START_ST = START_ST
+        self.END_ST = END_ST
 
-while True:
-    os.system('cls||clear')
+        self.setup_api()
 
-    res = client.service.GetDepBoardWithDetails(numRows=10, crs=START_ST, filterCrs=END_ST, timeOffset=0, timeWindow=119, _soapheaders=[header_value])
-    displayStationBoard(res,END_ST)
+    def setup_api(self):
+        load_dotenv()
 
-    res = client.service.GetDepBoardWithDetails(numRows=10, crs=END_ST, filterCrs=START_ST, timeOffset=0, timeWindow=119,   _soapheaders=[header_value])
-    displayStationBoard(res,START_ST)
-    line = f"Last updated: {res.generatedAt}"
-    if in_between(datetime.datetime.now().time(),datetime.time(7),datetime.time(9)):
-       line += " - Next update in 30s"
-       sleepTime = 30
-    elif in_between(datetime.datetime.now().time(),datetime.time(16),datetime.time(19)):
-       line += " - Next update in 30s"
-       sleepTime = 30
-    else:
-       line += " - Next update in 5m"
-       sleepTime = 60*5
-    print(line)
-    time.sleep(sleepTime)
+        LDB_TOKEN = os.getenv("RAIL_TOKEN")
+        WSDL = 'http://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx?ver=2021-11-01'
+
+        if LDB_TOKEN == '':
+            raise Exception("Please configure your OpenLDBWS token in getDepartureBoardExample!")
+
+        settings = Settings(strict=False)
+
+        history = HistoryPlugin()
+
+        self.client = Client(wsdl=WSDL, settings=settings, plugins=[history])
+
+        header = xsd.Element(
+            '{http://thalesgroup.com/RTTI/2013-11-28/Token/types}AccessToken',
+            xsd.ComplexType([
+                xsd.Element(
+                    '{http://thalesgroup.com/RTTI/2013-11-28/Token/types}TokenValue',
+                    xsd.String()),
+            ])
+        )
+        self.header_value = header(TokenValue=LDB_TOKEN)
+
+    def run(self):
+        while True:
+            os.system('cls||clear')
+
+            res = self.client.service.GetDepBoardWithDetails(numRows=10, crs=self.START_ST, filterCrs=self.END_ST, timeOffset=0, timeWindow=119, _soapheaders=[self.header_value])
+            displayStationBoard(res,self.END_ST)
+
+            res = self.client.service.GetDepBoardWithDetails(numRows=10, crs=self.END_ST, filterCrs=self.START_ST, timeOffset=0, timeWindow=119,   _soapheaders=[self.header_value])
+            displayStationBoard(res,self.START_ST)
+            line = f"Last updated: {res.generatedAt}"
+            if in_between(datetime.datetime.now().time(),datetime.time(7),datetime.time(9)):
+                line += " - Next update in 30s"
+                sleepTime = 30
+            elif in_between(datetime.datetime.now().time(),datetime.time(16),datetime.time(19)):
+                line += " - Next update in 30s"
+                sleepTime = 30
+            else:
+                line += " - Next update in 5m"
+                sleepTime = 60*5
+            print(line)
+            time.sleep(sleepTime)
+
+@click.command()
+@click.option('--start-st',default='SWT',help="Starting station code e.g SWT MAN")
+@click.option('--end-st',default='MAN', help="Destination statino code e.g LMS MAN")
+def main(start_st,end_st):
+    """
+    Show a departure board
+
+    Provide OpenLDBWS API token as env var: RAIL_TOKEN
+
+    
+    """
+    dash = Dashboard(start_st,end_st)
+    dash.run()
+
+if __name__ == "__main__":
+    main()
+   
